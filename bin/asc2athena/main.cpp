9091321255dfc707cb52ad593bdba940136388e8
Tom Schoonjans <Tom.Schoonjans@bam.de>
2015-08-20 10:15:10 +0200

    Code restructuring: introduced common convencience library

--- bin/asc2athena/main.cpp
+++ bin/asc2athena/main.cpp
@@ -9,18 +9,17 @@
 int main(int argc, char **argv) {
 	Glib::set_application_name("asc2athena");
 #if defined(G_OS_WIN32)
-        setlocale(LC_ALL,"English_United States");
+  setlocale(LC_ALL,"English_United States");
 	//set PLPLOT_LIB
 	gchar *installation_dir = g_win32_get_package_installation_directory_of_module(NULL);
 	std::string path_to_plplot(Glib::build_filename(installation_dir, "Share", "plplot"));
-	std::cout << "path to plplot: " << path_to_plplot << std::endl;
 	Glib::setenv("PLPLOT_LIB", path_to_plplot, true);
 	g_free(installation_dir);
 #else
-        Glib::setenv("LANG","en_US", true);
+  Glib::setenv("LANG","en_US", true);
 #endif
-        gtk_disable_setlocale();
-        setbuf(stdout,NULL);
+  gtk_disable_setlocale();
+  setbuf(stdout,NULL);
 	Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv, "de.bam.asc2athena");
 
 	Asc2AthenaWindow window;
