0f395a7b88e6bea6671f9330860d586901417ccb
Tom Schoonjans <Tom.Schoonjans@bam.de>
2015-08-21 21:00:47 +0200

    chi2dat: plplot fix

--- bin/chi2dat/main.cpp
+++ bin/chi2dat/main.cpp
@@ -9,13 +9,17 @@
 int main(int argc, char **argv) {
 	Glib::set_application_name("chi2dat");
 #if defined(G_OS_WIN32)
-        setlocale(LC_ALL,"English_United States");
-        //g_setenv("LANG","en_US",TRUE);
+  setlocale(LC_ALL,"English_United States");
+	gchar *installation_dir = g_win32_get_package_installation_directory_of_module(NULL);
+	std::string path_to_plplot(Glib::build_filename(installation_dir, "Share", "plplot"));
+	std::cout << "path to plplot: " << path_to_plplot << std::endl;
+	Glib::setenv("PLPLOT_LIB", path_to_plplot, true);
+	g_free(installation_dir);
 #else
-        Glib::setenv("LANG","en_US",TRUE);
+  Glib::setenv("LANG","en_US",TRUE);
 #endif
-        gtk_disable_setlocale();
-        setbuf(stdout,NULL);
+  gtk_disable_setlocale();
+  setbuf(stdout,NULL);
 	Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv, "de.bam.chi2dat");
 
 	Chi2DatWindow window;
